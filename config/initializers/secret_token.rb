34d4442dec1e9a7c11a62cf0edda13ca4fa9ff9e
truncs <sarawgi.aditya@gmail.com>
2011-12-04 04:42:56 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newslizr::Application.config.secret_token = '4d5a84f60b32b5268f94f3902b98d49fda02cfbf9d13c5b5807420bd02d50b46db8cf5dfcdc086d79822ba4a6d06d6c510eef2a4667350ef7e04a18ea6d0bc30'
